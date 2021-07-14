8454444a2741c84811fcb3f10b22e65b36deb197
Zaky C <82917438+ZakyC@users.noreply.github.com>
2021-07-14 10:18:23 +0700

    Update Details.cs

--- Details.cs
+++ Details.cs
@@ -2,20 +2,20 @@ using System;
 namespace Details
 {
    public class Program
-    {
+   {
        public static void Main(string[] args)
-        {
+       {
             string ProjectName = "Calculator";
             string DescProject = "C# terminal Calculator";
             string AuthorsName = "ZakyC";
             string ProjectLang = "C# - C SHARP";
             string DevicesUsed = "Windows 10 RAM 16GB, ROM 256GB";
             //Print the details
-            System.ConsoleWriteLine(ProjectName);
-            System.ConsoleWriteLine(DescProject);
-            System.ConsoleWriteLine(AuthorsName);
-            System.ConsoleWriteLine(ProjectLang);
-            System.ConsoleWriteLine(DevicesUsed);
+            System.Console.WriteLine(ProjectName);
+            System.Console.WriteLine(DescProject);
+            System.Console.WriteLine(AuthorsName);
+            System.Console.WriteLine(ProjectLang);
+            System.Console.WriteLine(DevicesUsed);
         }
     }
 }
