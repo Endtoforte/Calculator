55746b268d6ec9c435107b783645c8252c779f52
Zaky C <82917438+ZakyC@users.noreply.github.com>
2021-07-14 10:20:33 +0700

    Update Calc.cs

--- codes/Calc.cs
+++ codes/Calc.cs
@@ -2,23 +2,23 @@ using System;
 
 namespace Calculator
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             // Declare variables and then initialize to zero.
             int num1 = 0; int num2 = 0;
 
             // Display title as the C# console calculator app.
-            Console.WriteLine("Console Calculator in C#\r");
-            Console.WriteLine("------------------------\n");
+            System.Console.WriteLine("Console Calculator in C#\r");
+            System.Console.WriteLine("------------------------\n");
 
             // Ask the user to type the first number.
-            Console.WriteLine("Type a number, and then press Enter");
+            System.Console.WriteLine("Type a number, and then press Enter");
             num1 = Convert.ToInt32(Console.ReadLine());
 
             // Ask the user to type the second number.
-            Console.WriteLine("Type another number, and then press Enter");
+            System.Console.WriteLine("Type another number, and then press Enter");
             num2 = Convert.ToInt32(Console.ReadLine());
 
             // Ask the user to choose an option.
